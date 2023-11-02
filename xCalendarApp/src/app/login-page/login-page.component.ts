9ee878f31c071134dde7735bcf760e7fe78b85d0
PetrusQn <paul.kuhn2019@gmx.de>
2023-11-02 12:18:23 +0100

    Created log in page
    Created simple user log in page frontend.

---
+++ xCalendarApp/src/app/login-page/login-page.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { FormControl, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatMenuModule } from '@angular/material/menu';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.scss'],
+  standalone: true,
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, NgIf, MatIconModule, MatButtonModule, MatCheckboxModule, MatMenuModule], 
+})
+export class LoginPageComponent 
+{
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  hide = true; 
+}
