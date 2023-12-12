f9dbed7036913867a54b183e6640d4841ad57436
PetrusQn <paul.kuhn2019@gmx.de>
2023-12-12 17:56:06 +0100

    Created welcomePageComponent for better userexperience

--- xCalendarApp/src/app/app-routing.module.ts
+++ xCalendarApp/src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import { LoginPageComponent } from './login-page/login-page.component';
 import { RegisterPageComponent } from './register-page/register-page.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' }, 
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' }, 
+  { path: 'welcome', component: WelcomePageComponent, title: 'xCalendar - Welcome' },
   { path: 'login', component: LoginPageComponent, title: 'xCalendar - Login' }, 
   { path: 'registration', component: RegisterPageComponent, title: 'xCalendar - Registration' },
   { path: 'home/:userMailAddress/:userPassword', component: LandingPageComponent, title: 'xCalendar - Home' },
